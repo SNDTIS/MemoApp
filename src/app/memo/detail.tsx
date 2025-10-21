2cc83448018a5da102ab3ef9a9b6a464e7632ede
Taishi Sonoda <78803365+SNDTIS@users.noreply.github.com>
2025-10-21 20:32:37 +0900

    styling memo detail

--- src/app/memo/detail.tsx
+++ src/app/memo/detail.tsx
@@ -8,19 +8,19 @@ const Detail = (): JSX.Element => {
     return (
         <View style={styles.container}>
             <Header />
-            <View>
-                <Text>買い物リスト</Text>
-                <Text>2025年10月20日 10:00</Text>
+            <View style={styles.memoHeader}>
+                <Text style={styles.memoTitle}>買い物リスト</Text>
+                <Text style={styles.memoDate}>2025年10月20日 10:00</Text>
             </View>
 
-            <ScrollView>
-                <Text>
+            <ScrollView style={styles.memoBody}>
+                <Text style={styles.memoBodyText}>
                     買い物リスト
-                    書体やレイアウトなどを確認するために用います。本文用なので使い方を間違えると不自然に見えることもありますので要注意
+                    書体やレイアウトなどを確認するために用います。本文用なので使い方を間違えると不自然に見えることもありますので要注意。
                 </Text>
             </ScrollView>
 
-            <CircleButton>+</CircleButton>
+            <CircleButton style={{ top: 160, bottom: 'auto'}}>+</CircleButton>
 
         </View>
     )
@@ -30,6 +30,33 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#ffffff'
+    },
+    memoHeader: {
+        backgroundColor: '#467FD3',
+        height: 96,
+        justifyContent: 'center',
+        paddingVertical: 24,
+        paddingHorizontal: 19
+    },
+    memoTitle: {
+        fontSize: 20,
+        lineHeight: 32,
+        fontWeight: 'bold',
+        color: '#ffffff'
+    },
+    memoDate: {
+        fontSize: 12,
+        lineHeight: 16,
+        color: '#ffffff'
+    },
+    memoBody: {
+        paddingVertical: 32,
+        paddingHorizontal: 27
+    },
+    memoBodyText: {
+        fontSize: 16,
+        lineHeight: 24,
+        color: '#000000'
     }
 })
 
