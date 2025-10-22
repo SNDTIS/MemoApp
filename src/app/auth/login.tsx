ab33587c1231dfbf96b56cb55470db6f82a12185
Taishi Sonoda <78803365+SNDTIS@users.noreply.github.com>
2025-10-22 21:26:04 +0900

    markup login

---
+++ src/app/auth/login.tsx
@@ -0,0 +1,32 @@
+import { JSX } from 'react'
+import { View, Text, TextInput, StyleSheet } from 'react-native'
+
+import Header from '../../components/Header'
+
+const Login = ():JSX.Element => {
+    return (
+        <View>
+            <Header />
+            <View>
+                <Text>Log In</Text>
+                <TextInput value='Email address'/>
+                <TextInput value='Password'/>
+                <View>
+                    <Text>Submit</Text>
+                </View>
+                <View>
+                    <Text>Not registered?</Text>
+                    <Text>Sign up here</Text>
+                </View>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create ({
+    container: {
+        flex: 1
+    }
+})
+
+export default Login
