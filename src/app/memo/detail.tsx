c50f7d0c1eccf6d7da81f42838661a00d7ddf1b5
Taishi Sonoda <78803365+SNDTIS@users.noreply.github.com>
2025-10-22 20:42:40 +0900

    enable modify icon fonts

--- src/app/memo/detail.tsx
+++ src/app/memo/detail.tsx
@@ -1,6 +1,6 @@
 import { JSX } from 'react'
 import { View, Text, ScrollView, StyleSheet } from 'react-native'
-import { Feather } from '@expo/vector-icons'
+import Icon from '../../components/Icon'
 
 import Header from '../../components/Header'
 import CircleButton from '../../components/CircleButton'
@@ -22,7 +22,7 @@ const Detail = (): JSX.Element => {
             </ScrollView>
 
             <CircleButton style={{ top: 160, bottom: 'auto'}}>
-                <Feather name='check' size={40}/>
+                <Icon name='pencil' size={40} color='#ffffff' />
             </CircleButton>
 
         </View>
