af7be5b845cbc6eeb2f77f74a1a72d63b9a04c18
Taishi Sonoda <78803365+SNDTIS@users.noreply.github.com>
2025-10-25 16:18:02 +0900

    useEffect

--- src/app/memo/list.tsx
+++ src/app/memo/list.tsx
@@ -1,11 +1,13 @@
 import { JSX } from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 import { router, useNavigation } from 'expo-router'
+import { useEffect } from 'react'
 
 import Icon from '../../components/Icon'
 
 import MemoListItem from '../../components/MemoListItem'
 import CircleButton from '../../components/CircleButton'
+import LogOutButton from '../../components/LogOutButton'
 
 const handlePress = (): void => {
     router.push('/memo/create')
@@ -13,9 +15,11 @@ const handlePress = (): void => {
 
 const List = (): JSX.Element => {
     const navigation = useNavigation()
-    navigation.setOptions({
-        headerRight: () => { return <Text>Test</Text> }
-    })
+    useEffect(() => {
+        navigation.setOptions({
+            headerRight: () => { return <LogOutButton /> }
+        })
+    }, [])
     return (
         <View style={styles.container}>
             <View>
