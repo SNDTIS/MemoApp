a3faba67759391dcf9cbe6812b019d4b2aefa58a
Taishi Sonoda <78803365+SNDTIS@users.noreply.github.com>
2025-10-26 15:18:06 +0900

    fix

--- src/components/MemoListItem.tsx
+++ src/components/MemoListItem.tsx
@@ -24,7 +24,7 @@ const handlePress = (id: string): void => {
             onPress: () => {
                 deleteDoc(ref)
                 .catch(() => {
-                    Alert.alert('Fialed Delete.')
+                    Alert.alert('Failed Delete.')
                 })
             }
         }
