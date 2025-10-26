622ebf0644c795ff960af50435787f0906e18884
Taishi Sonoda <78803365+SNDTIS@users.noreply.github.com>
2025-10-26 13:07:36 +0900

    get data and monitoring

--- src/app/memo/list.tsx
+++ src/app/memo/list.tsx
@@ -2,12 +2,14 @@ import { JSX } from 'react'
 import { View, StyleSheet } from 'react-native'
 import { router, useNavigation } from 'expo-router'
 import { useEffect } from 'react'
+import { collection, onSnapshot, query, orderBy } from 'firebase/firestore'
 
 import Icon from '../../components/Icon'
 
 import MemoListItem from '../../components/MemoListItem'
 import CircleButton from '../../components/CircleButton'
 import LogOutButton from '../../components/LogOutButton'
+import { db, auth } from '../config'
 
 const handlePress = (): void => {
     router.push('/memo/create')
@@ -19,6 +21,17 @@ const List = (): JSX.Element => {
         navigation.setOptions({
             headerRight: () => { return <LogOutButton /> }
         })
+    }, [])
+    useEffect(() => {
+        if (auth.currentUser === null) { return }
+        const ref = collection(db, `users/${auth.currentUser?.uid}/memos`)
+        const q = query(ref, orderBy('updatedAt', 'desc'))
+        const unsubscribe = onSnapshot(q,(snapShot) => {
+            snapShot.forEach((doc) => {
+                console.log('memo',doc.data())
+            })
+        })
+        return unsubscribe
     }, [])
     return (
         <View style={styles.container}>
